4943d7528273884a47c6449957a265c69b9e2822
as13af <alishidqie.asaf@gmail.com>
2024-01-25 13:28:55 +0700

    Changes
    Add updated dependecies and android

--- app/src/main/java/com/example/celeryhydroponic/UltrasonicSensor.java
+++ app/src/main/java/com/example/celeryhydroponic/UltrasonicSensor.java
@@ -2,7 +2,6 @@ package com.example.celeryhydroponic;
 
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.annotation.Nullable;  // Add this import
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UltrasonicSensor extends AppCompatActivity {
@@ -15,7 +14,7 @@ public class UltrasonicSensor extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ultrasonic_sensor);
 
